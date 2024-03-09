383b299e5fae22658c727265393b9d5b93d1c5ab
mmisiewiczz <marta.misiewicz0@gmail.com>
2024-03-09 18:09:57 +0100

    Created console program sum_of_multiples

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int sum_of_multiples(int n) {
+    if (n < 0) {
+        return 0;
+    }
+
+    int total_sum = 0;
+    for (int i = 3; i < n; i += 3) {
+        total_sum += i;
+    }
+    for (int i = 5; i < n; i += 5) {
+        if (i % 3 != 0) {
+            total_sum += i;
+        }
+    }
+
+    return total_sum;
+}
+
+int main() {
+    // Testing the function for the number 10
+    printf("%d\n", sum_of_multiples(10));  // It should return 23
+
+    return 0;
+}
